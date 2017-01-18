ed1306275556eac1e62eaf165653c49112798739
phobos <phobos@master>
2017-01-18 13:20:19 +0100

    Activation du module reseau

--- manifests/config.pp
+++ manifests/config.pp
@@ -18,47 +18,47 @@ class openstack::config inherits openstack
                     "/etc/issue.net" :
                     content => template("openstack/messages/issue.net.erb");   
   
-                    #"/etc/motd" :
-                    #content   => template("openstack/messages/motd.erb"); 
+                    "/etc/motd" :
+                    content => template("openstack/messages/motd.erb"); 
                                            			        
 				            ###### NETWORK #####                
                     # /etc/services: 
                     # http://www.iana.org/assignments/port-numbers
                     # port (entre 0 et 1023) sont réservés pour les applications « standards 
                     # port pour les applications développées par les utilisateurs (1024 à 65535).
-                    #"/etc/services" :
-                    #content => template("openstack/network/services.erb");
+                    "/etc/services" :
+                    content => template("openstack/network/services.erb");
 
                     # /etc/sysctl.conf: 
                     #  Paramètres par défaut du système #vivent dans /usr/lib/sysctl.d/00-openstack.conf.
-                    #"/etc/sysctl.conf" :
-                    #content => template("openstack/network/sysctl.erb");
+                    "/etc/sysctl.conf" :
+                    content => template("openstack/network/sysctl.erb");
                     
                     # /etc/sysconfig/network (/etc/hostname):
-                    #"/etc/sysconfig/network" :
-                    #content => template("openstack/network/network.erb");
+                    "/etc/sysconfig/network" :
+                    content => template("openstack/network/network.erb");
              
                     # /etc/networks :
                     # Permet d'affecter un nom logique à un réseau (definition des domains)
                     # permet par exemple d'adresser un réseau (route) sur son nom, plutôt que sur son adresse.
-                    #"/etc/networks" :
-                    #content => template("openstack/network/networks.erb");
+                    "/etc/networks" :
+                    content => template("openstack/network/networks.erb");
                     
                     # /etc/host.conf:
                     # Donne l'ordre dans lequel le processus de résolution de noms est effectué.
-                    #"/etc/host.conf" :
-                    #content => template("openstack/network/host.conf.erb");
+                    "/etc/host.conf" :
+                    content => template("openstack/network/host.conf.erb");
                     
                     # /etc/hosts: 
                     # Moyen d'assurer la résolution de noms, de donner un nom FQDN à un hôte
-                    #"/etc/hosts" :
-                    #content => template("openstack/network/hosts.erb");
+                    "/etc/hosts" :
+                    content => template("openstack/network/hosts.erb");
                      
                     # /etc/resolv.conf:
                     # Permet d'affecter les serveurs de noms. 
                     # ATTENTION !!! Il peut etre configurer via DHCP
-                    #"/etc/resolv.conf" :
-                    #content => template("openstack/network/resolv.erb");
+                    "/etc/resolv.conf" :
+                    content => template("openstack/network/resolv.erb");
 
                     # Configuration des interfaces reseaux:
                     # Configuration /etc/sysconfig/network-scripts/ifcfg-eth0:
